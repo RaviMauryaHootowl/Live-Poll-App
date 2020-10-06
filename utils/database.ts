51e6347a054ca4f65b96fae07c617914e755f755
RaviMauryaHootowl <ravimaurya027@gmail.com>
2020-10-06 14:10:01 +0530

    bug fixes

--- utils/database.ts
+++ utils/database.ts
@@ -1,6 +1,5 @@
 import {MongoClient} from 'mongodb';
 
-const DATABASE_URL = "mongodb+srv://ravi:ravi@cluster0.kvkuv.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
